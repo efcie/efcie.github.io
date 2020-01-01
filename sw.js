fb24c9876eff0d8ed12302289a207b23a8781c75
Weilei Zeng <240155787@qq.com>
2019-12-31 17:34:57 -0800

    use an empty file to fix error from google analytics
    [2019-12-31 17:21:34] ERROR `/sw.js' not found.

---
+++ sw.js
@@ -0,0 +1,2 @@
+//this is an empty file and supposed to be deleted. I suggest it would be provided by Google Analytics.
+//I put this file here, because when I run it locally, it produces error: [2019-12-31 17:21:34] ERROR `/sw.js' not found.
