55f23b622999314c598cc30de2e0d0564d7d7fbf
Weilei Zeng <240155787@qq.com>
2020-04-13 14:27:41 -0700

    change root folder

--- _site/sw.js
+++
@@ -1,2 +0,0 @@
-//this is an empty file and supposed to be deleted. I suggest it would be provided by Google Analytics.
-//I put this file here, because when I run it locally, it produces error: [2019-12-31 17:21:34] ERROR `/sw.js' not found.
